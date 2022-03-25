d75bf273390c9a000a87d5647174a1753b599401
Noud63 <noudvandun@gmail.com>
2022-03-25 07:11:51 +0100

    test

--- backend/server.js
+++ backend/server.js
@@ -17,10 +17,10 @@ dotenv.config()
 connectDB()
 addDataToCollection()
 
-app.use(express.static(path.resolve(__dirname, "./frontend/build")))
+app.use(express.static(path.join(__dirname, '../frontend', 'build')))
 
-app.get('/*', (req, res) => {
-   res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+app.get('*', (req, res) => {
+   res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
 });
 
 
