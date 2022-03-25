e3bc45d1fc3cc3796fef3f420dc94738a35ef4ec
Noud63 <noudvandun@gmail.com>
2022-03-25 08:14:30 +0100

    test

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const dotenv = require('dotenv')
 const router = express.Router()
 const path = require("path");
 const colors = require('colors')
-const PORT = process.env.REACT_APP_PORT || 5000
+const PORT = process.env.PORT || 5000
 const connectDB = require('./config/db')
 const addDataToCollection = require('./seeder')
 
