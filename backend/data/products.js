80a4c588f09764fb259b7fbb2573471200cc07cc
Noud63 <noudvandun@gmail.com>
2022-07-19 12:26:21 +0200

    whatever

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,180 +1,180 @@
-const products = [
-    {
-        "artNr": "P1D",
-        "name": "D.I.S.C.O.",
-        "technique": "Acrylic on canvas",
-        "price": 6950,
-        "instock": 100,
-        "imgSrc": "/img/disco.jpg",
-        "imgBig": "/img/discofullsize.jpg",
-        "numberOfUnits": 1,
-        "like": false
-    },
-    {
-        "artNr": "P8E",
-        "name": "E.N.I.G.M.A.",
-        "technique": "Acrylic on canvas",
-        "price": 8950,
-        "instock": 43,
-        "imgSrc": "/img/enigma.jpg",
-        "imgBig": "/img/enigmafullsize.jpg",
-        "numberOfUnits": 1,
-        "like": false
-    },
-    {
-        "artNr": "P9H",
-        "name": "H.O.T.E.L.",
-        "technique": "Acrylic on canvas",
-        "price": 9000,
-        "instock": 100,
-        "imgSrc": "/img/hotel.jpg",
-        "imgBig": "/img/hotelfullsize.jpg",
-        "numberOfUnits": 1,
-        "like": false
-    },
-    {
-        "artNr": "P7U",
-        "name": "U.F.O.",
-        "technique": "Acrylic on canvas",
-        "price": 7950,
-        "instock": 100,
-        "imgSrc": "/img/ufo.jpg",
-        "imgBig": "/img/ufofullsize.jpg",
-        "numberOfUnits": 1,
-        "like": false
-    },
-    {
-        "artNr": "P69PA",
-        "name": "P.A.N.O.R.a.M.A",
-        "technique": "Acrylic on canvas",
-        "price": 6950,
-        "instock": 100,
-        "imgSrc": "/img/panorama.jpg",
-        "imgBig": "/img/panoramafullsize.jpg",
-        "numberOfUnits": 1,
-        "like": false
-    },
-    {
-        "artNr": "P10T",
-        "name": "The Expansion of the Universe II",
-        "technique": "Acrylic on canvas",
-        "price": 10950,
-        "instock": 100,
-        "imgSrc": "/img/teotu2.jpg",
-        "imgBig": "/img/teotu2fullsize.jpg",
-        "numberOfUnits": 1,
-        "like": false
-    },
-    {
-        "artNr": "P8TA",
-        "name": "Taxonomy/Entropy",
-        "technique": "Acrylic on canvas",
-        "price": 8950,
-        "instock": 100,
-        "imgSrc": "/img/taxonomy.jpg",
-        "imgBig": "/img/taxonomyfullsize.jpg",
-        "numberOfUnits": 1,
-        "like": false
-    },
-    {
-        "artNr": "P90P",
-        "name": "Patchwork",
-        "technique": "Acrylic on canvas",
-        "price": 9050,
-        "instock": 100,
-        "imgSrc": "/img/patchwork.jpg",
-        "imgBig": "/img/patchworkfullsize.jpg",
-        "numberOfUnits": 1,
-        "like": false
-    },
-    {
-        "artNr": "P9TE",
-        "name": "The Expansion of the Universe III",
-        "technique": "Acrylic on canvas",
-        "price": 9900,
-        "instock": 100,
-        "imgSrc": "/img/teotu3.jpg",
-        "imgBig": "/img/teotu3fullsize.jpg",
-        "numberOfUnits": 1,
-        "like": false
-    },
-    {
-        "artNr": "P99T",
-        "name": "The Present",
-        "technique": "Acrylic on canvas",
-        "price": 9958,
-        "instock": 100,
-        "imgSrc": "/img/thepresent.jpg",
-        "imgBig": "/img/thepresentfullsize.jpg",
-        "numberOfUnits": 1,
-        "like": false
-    },
-    {
-        "artNr": "P9C",
-        "name": "C.O.W.",
-        "technique": "Acrylic on canvas",
-        "price": 9999,
-        "instock": 100,
-        "imgSrc": "/img/cow.jpg",
-        "imgBig": "/img/cowfullsize.jpg",
-        "numberOfUnits": 1,
-        "like": false
-    },
-    {
-        "artNr": "P10M",
-        "name": "Mind the Gap",
-        "technique": "Mixed media on canvas",
-        "price": 10000,
-        "instock": 100,
-        "imgSrc": "/img/mindthegap.jpg",
-        "imgBig": "/img/mindthegapfullsize.jpg",
-        "numberOfUnits": 1,
-        "like": false
-    },
-    {
-        "artNr": "P79TE",
-        "name": "The Expansion of the Universe I",
-        "technique": "Acrylic on canvas",
-        "price": 7900,
-        "instock": 100,
-        "imgSrc": "/img/teotu.jpg",
-        "imgBig": "/img/teotufullsize.jpg",
-        "numberOfUnits": 1,
-        "like": false
-    },
-    {
-        "artNr": "P98D",
-        "name": "D.I.S.C.O.",
-        "technique": "Acrylic on canvas",
-        "price": 9858,
-        "instock": 100,
-        "imgSrc": "/img/disco2.jpg",
-        "imgBig": "/img/disco2fullsize.jpg",
-        "numberOfUnits": 1,
-        "like": false
-    },
-    {
-        "artNr": "P9R",
-        "name": "Reality for Beginners",
-        "technique": "Acrylic on canvas",
-        "price": 9999,
-        "instock": 100,
-        "imgSrc": "/img/gttp.jpg",
-        "imgBig": "/img/gttpfullsize.jpg",
-        "numberOfUnits": 1,
-        "like": false
-    },
-    {
-        "artNr": "P105M",
-        "name": "Mind the Gap",
-        "technique": "Mixed media on canvas",
-        "price": 10500,
-        "instock": 100,
-        "imgSrc": "/img/mindthegap2.jpg",
-        "imgBig": "/img/mindthegap2fullsize.jpg",
-        "numberOfUnits": 1,
-        "like": false
-    }
-]
+// const products = [
+//     {
+//         "artNr": "P1D",
+//         "name": "D.I.S.C.O.",
+//         "technique": "Acrylic on canvas",
+//         "price": 6950,
+//         "instock": 100,
+//         "imgSrc": "/img/disco.jpg",
+//         "imgBig": "/img/discofullsize.jpg",
+//         "numberOfUnits": 1,
+//         "like": false
+//     },
+//     {
+//         "artNr": "P8E",
+//         "name": "E.N.I.G.M.A.",
+//         "technique": "Acrylic on canvas",
+//         "price": 8950,
+//         "instock": 43,
+//         "imgSrc": "/img/enigma.jpg",
+//         "imgBig": "/img/enigmafullsize.jpg",
+//         "numberOfUnits": 1,
+//         "like": false
+//     },
+//     {
+//         "artNr": "P9H",
+//         "name": "H.O.T.E.L.",
+//         "technique": "Acrylic on canvas",
+//         "price": 9000,
+//         "instock": 100,
+//         "imgSrc": "/img/hotel.jpg",
+//         "imgBig": "/img/hotelfullsize.jpg",
+//         "numberOfUnits": 1,
+//         "like": false
+//     },
+//     {
+//         "artNr": "P7U",
+//         "name": "U.F.O.",
+//         "technique": "Acrylic on canvas",
+//         "price": 7950,
+//         "instock": 100,
+//         "imgSrc": "/img/ufo.jpg",
+//         "imgBig": "/img/ufofullsize.jpg",
+//         "numberOfUnits": 1,
+//         "like": false
+//     },
+//     {
+//         "artNr": "P69PA",
+//         "name": "P.A.N.O.R.a.M.A",
+//         "technique": "Acrylic on canvas",
+//         "price": 6950,
+//         "instock": 100,
+//         "imgSrc": "/img/panorama.jpg",
+//         "imgBig": "/img/panoramafullsize.jpg",
+//         "numberOfUnits": 1,
+//         "like": false
+//     },
+//     {
+//         "artNr": "P10T",
+//         "name": "The Expansion of the Universe II",
+//         "technique": "Acrylic on canvas",
+//         "price": 10950,
+//         "instock": 100,
+//         "imgSrc": "/img/teotu2.jpg",
+//         "imgBig": "/img/teotu2fullsize.jpg",
+//         "numberOfUnits": 1,
+//         "like": false
+//     },
+//     {
+//         "artNr": "P8TA",
+//         "name": "Taxonomy/Entropy",
+//         "technique": "Acrylic on canvas",
+//         "price": 8950,
+//         "instock": 100,
+//         "imgSrc": "/img/taxonomy.jpg",
+//         "imgBig": "/img/taxonomyfullsize.jpg",
+//         "numberOfUnits": 1,
+//         "like": false
+//     },
+//     {
+//         "artNr": "P90P",
+//         "name": "Patchwork",
+//         "technique": "Acrylic on canvas",
+//         "price": 9050,
+//         "instock": 100,
+//         "imgSrc": "/img/patchwork.jpg",
+//         "imgBig": "/img/patchworkfullsize.jpg",
+//         "numberOfUnits": 1,
+//         "like": false
+//     },
+//     {
+//         "artNr": "P9TE",
+//         "name": "The Expansion of the Universe III",
+//         "technique": "Acrylic on canvas",
+//         "price": 9900,
+//         "instock": 100,
+//         "imgSrc": "/img/teotu3.jpg",
+//         "imgBig": "/img/teotu3fullsize.jpg",
+//         "numberOfUnits": 1,
+//         "like": false
+//     },
+//     {
+//         "artNr": "P99T",
+//         "name": "The Present",
+//         "technique": "Acrylic on canvas",
+//         "price": 9958,
+//         "instock": 100,
+//         "imgSrc": "/img/thepresent.jpg",
+//         "imgBig": "/img/thepresentfullsize.jpg",
+//         "numberOfUnits": 1,
+//         "like": false
+//     },
+//     {
+//         "artNr": "P9C",
+//         "name": "C.O.W.",
+//         "technique": "Acrylic on canvas",
+//         "price": 9999,
+//         "instock": 100,
+//         "imgSrc": "/img/cow.jpg",
+//         "imgBig": "/img/cowfullsize.jpg",
+//         "numberOfUnits": 1,
+//         "like": false
+//     },
+//     {
+//         "artNr": "P10M",
+//         "name": "Mind the Gap",
+//         "technique": "Mixed media on canvas",
+//         "price": 10000,
+//         "instock": 100,
+//         "imgSrc": "/img/mindthegap.jpg",
+//         "imgBig": "/img/mindthegapfullsize.jpg",
+//         "numberOfUnits": 1,
+//         "like": false
+//     },
+//     {
+//         "artNr": "P79TE",
+//         "name": "The Expansion of the Universe I",
+//         "technique": "Acrylic on canvas",
+//         "price": 7900,
+//         "instock": 100,
+//         "imgSrc": "/img/teotu.jpg",
+//         "imgBig": "/img/teotufullsize.jpg",
+//         "numberOfUnits": 1,
+//         "like": false
+//     },
+//     {
+//         "artNr": "P98D",
+//         "name": "D.I.S.C.O.",
+//         "technique": "Acrylic on canvas",
+//         "price": 9858,
+//         "instock": 100,
+//         "imgSrc": "/img/disco2.jpg",
+//         "imgBig": "/img/disco2fullsize.jpg",
+//         "numberOfUnits": 1,
+//         "like": false
+//     },
+//     {
+//         "artNr": "P9R",
+//         "name": "Reality for Beginners",
+//         "technique": "Acrylic on canvas",
+//         "price": 9999,
+//         "instock": 100,
+//         "imgSrc": "/img/gttp.jpg",
+//         "imgBig": "/img/gttpfullsize.jpg",
+//         "numberOfUnits": 1,
+//         "like": false
+//     },
+//     {
+//         "artNr": "P105M",
+//         "name": "Mind the Gap",
+//         "technique": "Mixed media on canvas",
+//         "price": 10500,
+//         "instock": 100,
+//         "imgSrc": "/img/mindthegap2.jpg",
+//         "imgBig": "/img/mindthegap2fullsize.jpg",
+//         "numberOfUnits": 1,
+//         "like": false
+//     }
+// ]
 
-module.exports = products
+// module.exports = products
