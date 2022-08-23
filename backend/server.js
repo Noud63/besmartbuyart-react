a92fdd1666cbb0229a81167b75085fbbf4e4f88a
Noud63 <noudvandun@gmail.com>
2022-08-23 14:28:38 +0200

    whatever

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ const colors = require('colors')
 const PORT = process.env.PORT || 5000
 const connectDB = require('./config/db')
 const addDataToCollection = require('./seeder')
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 
 app.use(cors())
 app.use(express.json())
@@ -32,6 +33,9 @@ app.use('/', express.static(path.join(__dirname, '../frontend', 'build')))
 app.get('/*', (req, res) => {
    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
 });
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(PORT, '0.0.0.0', () => {
    console.log(`Server running on port ${PORT}`.yellow)
