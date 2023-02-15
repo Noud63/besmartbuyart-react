01c76bd44fb30114d16d3b7486c5004e05881f78
Noud63 <noudvandun@gmail.com>
2023-02-15 13:01:32 +0100

    test

--- backend/server.js
+++ backend/server.js
@@ -32,25 +32,25 @@ app.use('/stripe', require('./routes/checkoutRoute'))
 
 // Place after routes
 // Heroku
-app.use('/', express.static(path.join(__dirname, '../frontend', 'public')))
+// app.use('/', express.static(path.join(__dirname, '../frontend', 'public')))
 
-app.get('/*', (req, res) => {
-   res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
-});
+// app.get('/*', (req, res) => {
+//    res.sendFile(path.resolve(__dirname, '../frontend/build/img'))
+// });
 
 //Cyclic
 //Serving the frontend
 
 
-// app.use('/', express.static(path.join(__dirname, "../frontend/build")))
+app.use('/', express.static(path.join("/besmartbuyartreact/frontend/build")))
 
-// app.get("*", (_, res) => {
-//      res.sendFile(path.join(__dirname, "../frontend/build/index.html"),
-//      function(err){
-//         res.status(500).send(err)
-//      }
-//      )
-// })
+app.get("*", function(req, res){
+     res.sendFile(path.join("/besmartbuyartreact/frontend/build/index.html"),
+     function(err){
+        res.status(500).send(err)
+     }
+     )
+})
 
 // app.use(notFound)
 // app.use(errorHandler)
