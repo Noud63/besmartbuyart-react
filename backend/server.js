3c3143826921bb3afe8832344e56a2df106bc4a2
Noud63 <noudvandun@gmail.com>
2023-02-15 13:55:16 +0100

    test

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ app.use('/stripe', require('./routes/checkoutRoute'))
 // Place after routes
 // Heroku
 
-app.use('/', express.static(path.join(__dirname, '../frontend', 'build')))
+app.use('/', express.static(path.join(__dirname, '../frontend/build')))
 
 app.get('/*', (req, res) => {
    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
