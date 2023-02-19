04bf5c0862afa0901350484187d8bee4cc368f92
Noud63 <noudvandun@gmail.com>
2023-02-19 13:33:59 +0100

    cyclic

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ dotenv.config()
 
 
 connectDB().then(() => {
-   app.listen(process.env.REACT_APP_PORT, '0.0.0.0', () => {
+   app.listen(process.env.REACT_APP_PORT, () => {
       console.log(`Server running on port ${process.env.REACT_APP_PORT}`.yellow)
    })
 })
