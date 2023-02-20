6953029a9458adefef8d5bc91ce26daf8f774e3f
Noud63 <noudvandun@gmail.com>
2023-02-20 14:30:01 +0100

    webpack

--- backend/controllers/productInfoController.js
+++ backend/controllers/productInfoController.js
@@ -4,7 +4,7 @@ const ProductInfos = require('../models/productInfoModel')
 
 const getProductInfo = async (req, res) => {
     const productinfos = await ProductInfos.find({})
-    res.send(productinfos)
+    res.json(productinfos)
 }
 
 module.exports = getProductInfo
