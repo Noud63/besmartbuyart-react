87d7d69543966dd4a2e3ed668021a0c9f5c8353e
Noud63 <noudvandun@gmail.com>
2023-02-20 08:46:12 +0100

    refactor

--- backend/server.js
+++ backend/server.js
@@ -17,11 +17,16 @@ app.use(cors())
 app.use(express.json())
 dotenv.config()
 
-connectDB()
+
+connectDB().then(() => {
+   app.listen(process.env.REACT_APP_PORT, () => {
+      console.log(`Server running on port ${process.env.REACT_APP_PORT}`.yellow)
+   })
+})
 
 // addDataToCollection()
 
-// app.use('/artworks', require('./routes/artworkRoute'))
+app.use('/artworks', require('./routes/artworkRoute'))
 app.use('/users', require('./routes/registerRoute'))
 app.use('/logins', require('./routes/loginRoute'))
 app.use('/productinfo', require('./routes/productInfoRoute'))
@@ -35,10 +40,6 @@ app.use('/', express.static(path.join(__dirname, '../frontend', 'build')))
 app.get('*', (req, res) => {
    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
 });
-
-app.listen(process.env.REACT_APP_PORT, () => {
-   console.log(`Server running on port ${process.env.REACT_APP_PORT}`.yellow)
-})
 
 // app.use(notFound)
 // app.use(errorHandler)
