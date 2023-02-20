63285a86334b98be08c52fb07be1a259590a9157
Noud63 <noudvandun@gmail.com>
2023-02-20 14:23:51 +0100

    webpack

--- backend/server.js
+++ backend/server.js
@@ -31,11 +31,11 @@ app.use('/stripe', require('./routes/checkoutRoute'))
 
 // Place after routes
 // Cyclic, Render => Serving the frontend
-// app.use('/', express.static(path.join(__dirname, '../frontend', 'build')))
+app.use('/', express.static(path.join(__dirname, '../frontend', 'build')))
 
-// app.get('*', (req, res) => {
-//    res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
-// });
+app.get('*', (req, res) => {
+   res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+});
 
 app.listen(process.env.REACT_APP_PORT, '0.0.0.0', () => {
    console.log(`Server running on port ${process.env.REACT_APP_PORT}`.yellow)
